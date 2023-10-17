417b92568889d291f0860e30ac45eb887481c373
GF3000 <94068411+GF3000@users.noreply.github.com>
2023-10-17 13:55:49 +0200

    Mejora en los comentarios

--- lexer.py
+++ lexer.py
@@ -3,14 +3,16 @@ import tokens
 
 class Token:
     def __init__(self, token_type, attribute, value):
+        #Inicializamos los atributos de cada token
         self.token_type = token_type
         self.attribute = attribute
         self.value = value
 
     def __str__(self):
-        if self.attribute == None:
+        #Cada token se imprime de la siguiente manera:
+        if self.attribute == None: #Sin atributo
             return f'< {self.token_type}, >'
-        else:
+        else:#Con atributo
             return f'< {self.token_type}, {self.attribute}>'
 
     def __repr__(self):
@@ -18,71 +20,72 @@ class Token:
 
 class Lexer:
     def __init__(self, tokens, source_code):
+        #Inicializamos los atributos del lexer
         self.tokens_lenguaje = tokens
         self.source_code = source_code
         self.position = 0
         self.desplazamiento = 0
         self.token_list = []
     def get_desplazamiento(self):
+        #Devuelve el desplazamiento de la tabla de simbolos, FALTA IMPLEMENTAR
         return self.desplazamiento
     def get_token_list(self):
+        #Devuelve la lista de tokens
         return self.token_list
     def analizar(self):
-        position = 0
-        while position < len(source_code):
-            match = None
-            for token_type, attribute, pattern in tokens_leguaje:
+        #Analiza el código fuente y devuelve la lista de tokens
+        position = 0 #Posición actual en el código fuente, es como un puntero
+        while position < len(source_code): #Mientras no se haya llegado al final del código fuente
+            match = None #Variable para saber si se ha encontrado un token
+            for token_type, attribute, pattern in tokens_leguaje: #Recorremos la lista de tokens de nuestro lenguaje
                 regex = re.compile(pattern)
                 match = regex.match(source_code, position)
-                if match:
-                    value = match.group(0)
-                    if token_type == 'INTEGER':
+                if match: #Si se ha encontrado un token
+                    value = match.group(0) #Tipo de token
+                    if token_type == 'INTEGER': #Si es un entero, lo convertimos a int
                         attribute = int(value)
-                    elif token_type == 'IDENTIFIER':
+                    elif token_type == 'IDENTIFIER': #Si es un identificador, lo añadimos a la tabla de simbolos, FALTA IMPLEMENTAR
                         attribute = self.desplazamiento
                         self.desplazamiento += 1 #Hay que cambiarlo por el desplazamiento de la tabla de simbolos
-                    elif token_type == 'CAD':
+                    elif token_type == 'CAD': #Si es una cadena, el atributo es el valor de la cadena 
                         attribute = value
-                    self.token_list.append(Token(token_type, attribute, value))
-                    position = match.end()
+                    self.token_list.append(Token(token_type, attribute, value)) #Añadimos el token a la lista de tokens
+                    position = match.end() #Actualizamos la posición actual al final de la coincidencia
                     break
                 
 
-            if not match:
+            if not match: #Si no se ha encontrado un token
                 #Nos aseguramos de que no sea un espacio en blanco
                 if source_code[position] == ' ' or source_code[position] == '\n' or source_code[position] == '\t':
                     position += 1
-                else:
+                else:#Si no es un espacio en blanco, es un caracter no reconocido
                     print(f'[-] Error en la posición {position} del código fuente')
                     print(f'[-] Caracter no reconocido: "{source_code[position]}"')
                     position += 1
-                    return []
+                    return [] #Forzamos a que devuelva una lista vacía
                 
         return self.token_list
 
 
 
-
-# Definición de tokens y sus expresiones regulares
-
-# Ejemplo de código fuente
-#source_code = 'if x > 5 while y < 10 + z "Hello, World" !true [ ] { } miVariable++ == tuVariable[5]] su_variable = 8+9'
-file = "in3.txt"
-#OPen file
+#Archivo a analizar
+file = "in4.txt"
+#Abrimos el archivo
 f = open(file, "r")
 source_code = f.read()
 
-# Llamada al analizador léxico
+#Cargamos los tokens
 tokens_leguaje = tokens.Tokens.tokens
+#Creamos el objeto mi_lexer
 mi_lexer = Lexer(tokens_leguaje, source_code)
 
-
+#Analizamos el código fuente
 tokens_analizados = mi_lexer.analizar()
-# Imprimir los tokens encontrados
-#Write tokens in file
+
+
 f = open("tokens.txt", "w")
 for token in tokens_analizados:
-    f.write(str(token))
+    f.write(str(token)) #Guardamos los tokens en un archivo
     f.write("\n")
-for token in tokens_analizados:
-    print(token)
+    print(token) #Imprimimos los tokens por consola
+

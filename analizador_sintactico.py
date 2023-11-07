a0056768a6f3577a25a71d9f0ba36cfb78b91164
GF3000 <94068411+GF3000@users.noreply.github.com>
2023-11-07 19:37:18 +0100

    Nueva estructura de datos

--- analizador_sintactico.py
+++ analizador_sintactico.py
@@ -1,3 +1,61 @@
+class OrderedSet:
+    """Implementación de un conjunto ordenado"""
+
+    def __init__(self, iterable=None):
+        self.data = []
+        self.set_data = set()
+        if iterable is not None:
+            self.update(iterable)
+
+    def add(self, item):
+        if item not in self.set_data:
+            self.data.append(item)
+            self.set_data.add(item)
+
+    def update(self, iterable):
+        for item in iterable:
+            self.add(item)
+
+    def remove(self, item):
+        if item in self.set_data:
+            self.data.remove(item)
+            self.set_data.remove(item)
+
+    def discard(self, item):
+        if item in self.set_data:
+            self.data.remove(item)
+            self.set_data.discard(item)
+
+    def clear(self):
+        self.data = []
+        self.set_data = set()
+
+    def __contains__(self, item):
+        return item in self.set_data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __repr__(self):
+        return repr(self.data)
+    
+    def __sub__(self, other):
+        if isinstance(other, OrderedSet) or isinstance(other, set):
+            new_ordered_set = OrderedSet(self.data)
+            for item in other:
+                new_ordered_set.discard(item)
+            return new_ordered_set
+        else:
+            raise TypeError("Unsupported operand type for -: 'OrderedSet' and {}".format(type(other)))
+
+
+        
+
+
+
 class syntaxAnalyzer:
 
     def __init__(self, grammar, first = None, follow = None):
@@ -20,7 +78,7 @@ class syntaxAnalyzer:
             return self.first[symbol]
 
         # Crear un conjunto vacío para el conjunto FIRST del símbolo no terminal
-        first_set = set()
+        first_set = OrderedSet()
 
         # Obtener las producciones del símbolo no terminal
         productions = grammar[symbol]
@@ -51,7 +109,7 @@ class syntaxAnalyzer:
             return self.follow[symbol]
 
         # Crear un conjunto vacío para el conjunto FOLLOW del símbolo no terminal
-        follow_set = set()
+        follow_set = OrderedSet()
 
         # El símbolo inicial tiene '$' en su conjunto FOLLOW
         if symbol == 'S':

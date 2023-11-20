791018897bb39d63c1cece9e7a6049fef3635aa7
GF3000 <94068411+GF3000@users.noreply.github.com>
2023-11-20 17:14:48 +0100

    analizador sintactico montado

---
+++ Parseador de Tablas/parseadorREGLAS.py
@@ -0,0 +1,21 @@
+#read reglas.txt
+
+with open('reglas.txt', 'r') as f:
+    reglas = f.readlines()
+
+contador = 1
+
+for regla in reglas:
+    linea = regla.split(' ')
+    #Remove = "" from each element
+    linea[-1] = linea[-1][:-1]
+    for elemento in linea:
+        if len(elemento) == 0:
+            linea.remove(elemento)
+        if elemento == 'lambda':
+            linea[linea.index(elemento)] = ' '
+    
+    print(f"{contador}: REGLA( '{linea[0]}', {linea[2:]}),")
+    with open('reglas_dict.txt', 'a') as f:
+        f.write(f"{contador}: REGLA( '{linea[0]}', {linea[2:]}),\n")
+    contador += 1
